1b307a5265af77728d0103d215ff5d80c66dcdd5
Kavya Hegde <kavyahegde2005@gmail.com>
2026-03-08 14:03:06 +0530

    routing fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,20 @@ export default function Navbar(props) {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarScroll">
       <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{ "--bs-scroll-height": "100px" }}>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="/">Link</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Link</a>
+          <a className="nav-link disabled" aria-disabled="true" href="/">Link</a>
         </li>
       </ul>
       <form className="d-flex" role="search">
